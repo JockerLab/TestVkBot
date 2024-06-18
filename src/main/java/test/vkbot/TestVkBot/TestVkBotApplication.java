186fb10e352bb1d9fd28aa79bbe7b31a2ff9e152
Vsevolod Shaldin <shaldin@g-soft.ru>
2024-06-18 22:45:42 +0300

    Init project

---
+++ src/main/java/test/vkbot/TestVkBot/TestVkBotApplication.java
@@ -0,0 +1,13 @@
+package test.vkbot.TestVkBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestVkBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestVkBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/vkbot/TestVkBot/TestVkBotApplicationTests.java
@@ -0,0 +1,13 @@
+package test.vkbot.TestVkBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestVkBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
